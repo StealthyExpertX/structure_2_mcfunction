5bad0c2ef47ed1f496f2aea884d793997c652d05
StealthyExpert <stealthyexpertx@gmail.com>
2019-11-12 11:45:34 -0500

    Fixed the issue with offsetting blocks  + leaves conversion
    Fixed the issue with offsetting blocks  + leaves conversion table/method was added just not implemented so the script can't run just yet but will soon.

--- structure_2_mcfunction.py
+++ structure_2_mcfunction.py
@@ -1,7 +1,7 @@
-# Structure to functions command
+# Structure to function command
 # V0.2
 # Created by dakonblackrose & StealthyExpert
-
+#Made some edits so it uses relative coordinates.
 from pymclevel import MCSchematic
 from pymclevel import TileEntity
 from pymclevel import TAG_Compound
@@ -27,261 +27,259 @@ from itertools import izip_longest
 
 script_path = os.path.dirname(__file__)
 
-#Java Ids 1.12 to Java 1.12 Function.
-names = {
-	"0": "air",
-	"1": "stone",
-	"2": "grass",
-	"3": "dirt",
-	"4": "cobblestone",
-	"5": "planks",
-	"6": "sapling",
-	"7": "bedrock",
-	"8": "flowing_water",
-	"9": "water",
-	"10": "flowing_lava",
-	"11": "lava",
-	"12": "sand",
-	"13": "gravel",
-	"14": "gold_ore",
-	"15": "iron_ore",
-	"16": "coal_ore",
-	"17": "log",
-	"18": "leaves",
-	"19": "sponge",
-	"20": "glass",
-	"21": "lapis_ore",
-	"22": "lapis_block",
-	"23": "dispenser",
-	"24": "sandstone",
-	"25": "noteblock",
-	"26": "bed",
-	"27": "golden_rail",
-	"28": "detector_rail",
-	"29": "sticky_piston",
-	"30": "web",
-	"31": "tallgrass",
-	"32": "deadbush",
-	"33": "piston",
-	"34": "piston_head",
-	"35": "wool",
-	"36": "piston_extension",
-	"37": "yellow_flower",
-	"38": "red_flower",
-	"39": "brown_mushroom",
-	"40": "red_mushroom",
-	"41": "gold_block",
-	"42": "iron_block",
-	"43": "double_stone_slab",
-	"44": "stone_slab",
-	"45": "brick_block",
-	"46": "tnt",
-	"47": "bookshelf",
-	"48": "mossy_cobblestone",
-	"49": "obsidian",
-	"50": "torch",
-	"51": "fire",
-	"52": "mob_spawner",
-	"53": "oak_stairs",
-	"54": "chest",
-	"55": "redstone_wire",
-	"56": "diamond_ore",
-	"57": "diamond_block",
-	"58": "crafting_table",
-	"59": "wheat",
-	"60": "farmland",
-	"61": "furnace",
-	"62": "lit_furnace",
-	"63": "standing_sign",
-	"64": "wooden_door",
-	"65": "ladder",
-	"66": "rail",
-	"67": "stone_stairs",
-	"68": "wall_sign",
-	"69": "lever",
-	"70": "stone_pressure_plate",
-	"71": "iron_door",
-	"72": "wooden_pressure_plate",
-	"73": "redstone_ore",
-	"74": "lit_redstone_ore",
-	"75": "unlit_redstone_torch",
-	"76": "redstone_torch",
-	"77": "stone_button",
-	"78": "snow_layer",
-	"79": "ice",
-	"80": "snow",
-	"81": "cactus",
-	"82": "clay",
-	"83": "reeds",
-	"84": "jukebox",
-	"85": "fence",
-	"86": "pumpkin",
-	"87": "netherrack",
-	"88": "soul_sand",
-	"89": "glowstone",
-	"90": "portal",
-	"91": "lit_pumpkin",
-	"92": "cake",
-	"93": "unpowered_repeater",
-	"94": "powered_repeater",
-	"95": "stained_glass",
-	"96": "trapdoor",
-	"97": "monster_egg",
-	"98": "stonebrick",
-	"99": "brown_mushroom_block",
-	"100": "red_mushroom_block",
-	"101": "iron_bars",
-	"102": "glass_pane",
-	"103": "melon_block",
-	"104": "pumpkin_stem",
-	"105": "melon_stem",
-	"106": "vine",
-	"107": "fence_gate",
-	"108": "brick_stairs",
-	"109": "stone_brick_stairs",
-	"110": "mycelium",
-	"111": "waterlily",
-	"112": "nether_brick",
-	"113": "nether_brick_fence",
-	"114": "nether_brick_stairs",
-	"115": "nether_wart",
-	"116": "enchanting_table",
-	"117": "brewing_stand",
-	"118": "cauldron",
-	"119": "end_portal",
-	"120": "end_portal_frame",
-	"121": "end_stone",
-	"122": "dragon_egg",
-	"123": "redstone_lamp",
-	"124": "lit_redstone_lamp",
-	"125": "double_wooden_slab",
-	"126": "wooden_slab",
-	"127": "cocoa",
-	"128": "sandstone_stairs",
-	"129": "emerald_ore",
-	"130": "ender_chest",
-	"131": "tripwire_hook",
-	"132": "tripwire",
-	"133": "emerald_block",
-	"134": "spruce_stairs",
-	"135": "birch_stairs",
-	"136": "jungle_stairs",
-	"137": "command_block",
-	"138": "beacon",
-	"139": "cobblestone_wall",
-	"140": "flower_pot",
-	"141": "carrots",
-	"142": "potatoes",
-	"143": "wooden_button",
-	"144": "skull",
-	"145": "anvil",
-	"146": "trapped_chest",
-	"147": "light_weighted_pressure_plate",
-	"148": "heavy_weighted_pressure_plate",
-	"149": "unpowered_comparator",
-	"150": "powered_comparator",
-	"151": "daylight_detector",
-	"152": "redstone_block",
-	"153": "quartz_ore",
-	"154": "hopper",
-	"155": "quartz_block",
-	"156": "quartz_stairs",
-	"157": "activator_rail",
-	"158": "dropper",
-	"159": "stained_hardened_clay",
-	"160": "stained_glass_pane",
-	"161": "leaves2",
-	"162": "log2",
-	"163": "acacia_stairs",
-	"164": "dark_oak_stairs",
-	"165": "slime",
-	"166": "barrier",
-	"167": "iron_trapdoor",
-	"168": "prismarine",
-	"169": "sea_lantern",
-	"170": "hay_block",
-	"171": "carpet",
-	"172": "hardened_clay",
-	"173": "coal_block",
-	"174": "packed_ice",
-	"175": "double_plant",
-	"176": "standing_banner",
-	"177": "wall_banner",
-	"178": "daylight_detector_inverted",
-	"179": "red_sandstone",
-	"180": "red_sandstone_stairs",
-	"181": "stone_slab2",
-	"182": "double_stone_slab2",
-	"183": "spruce_fence_gate",
-	"184": "birch_fence_gate",
-	"185": "jungle_fence_gate",
-	"186": "dark_oak_fence_gate",
-	"187": "acacia_fence_gate",
-	"188": "spruce_fence",
-	"189": "birch_fence",
-	"190": "jungle_fence",
-	"191": "dark_oak_fence",
-	"192": "acacia_fence",
-	"193": "spruce_door",
-	"194": "birch_door",
-	"195": "jungle_door",
-	"196": "acacia_door",
-	"197": "dark_oak_door",
-	"198": "end_rod",
-	"199": "none",
-	"200": "chorus_flower",
-	"201": "purpur_block",
-	"202": "purpur_pillar",
-	"203": "purpur_stairs",
-	"204": "purpur_double_slab",
-	"205": "purpur_slab",
-	"206": "end_bricks",
-	"208": "grass_path",
-	"209": "end_gateway",
-	"210": "repeating_command_block",
-	"211": "chain_command_block",
-	"212": "frosted_ice",
-	"213": "magma",
-	"214": "nether_wart_block",
-	"215": "red_nether_brick",
-	"216": "bone_block",
-	"217": "structure_void",
-	"218": "shulker_box",
-	"219": "white_shulker_box",
-	"220": "orange_shulker_box",
-	"221": "magenta_shulker_box",
-	"222": "light_blue_shulker_box",
-	"223": "yellow_shulker_box",
-	"224": "lime_shulker_box",
-	"225": "pink_shulker_box",
-	"226": "gray_shulker_box",
-	"227": "silver_shulker_box",
-	"228": "cyan_shulker_box",
-	"229": "purple_shulker_box",
-	"230": "blue_shulker_box",
-	"231": "brown_shulker_box",
-	"232": "green_shulker_box",
-	"233": "red_shulker_box",
-	"234": "black_shulker_box",
-	"235": "white_glazed_terracotta",
-	"236": "orange_glazed_terracotta",
-	"237": "magenta_glazed_terracotta",
-	"238": "light_blue_glazed_terracotta",
-	"239": "yellow_glazed_terracotta",
-	"240": "lime_glazed_terracotta",
-	"241": "pink_glazed_terracotta",
-	"242": "gray_glazed_terracotta",
-	"243": "silver_glazed_terracotta",
-	"244": "cyan_glazed_terracotta",
-	"245": "purple_glazed_terracotta",
-	"246": "blue_glazed_terracotta",
-	"247": "brown_glazed_terracotta",
-	"248": "green_glazed_terracotta",
-	"249": "red_glazed_terracotta",
-	"250": "black_glazed_terracotta",
-	"251": "concrete",
-	"252": "concrete_powder",
-	"255": "structure_block",
+names = {"0": "air",
+		"1": "stone",
+		"2": "grass",
+		"3": "dirt",
+		"4": "cobblestone",
+		"5": "planks",
+		"6": "sapling",
+		"7": "bedrock",
+		"8": "flowing_water",
+		"9": "water",
+		"10": "flowing_lava",
+		"11": "lava",
+		"12": "sand",
+		"13": "gravel",
+		"14": "gold_ore",
+		"15": "iron_ore",
+		"16": "coal_ore",
+		"17": "log",
+		"18": "leaves",
+		"19": "sponge",
+		"20": "glass",
+		"21": "lapis_ore",
+		"22": "lapis_block",
+		"23": "dispenser",
+		"24": "sandstone",
+		"25": "noteblock",
+		"26": "bed",
+		"27": "golden_rail",
+		"28": "detector_rail",
+		"29": "sticky_piston",
+		"30": "web",
+		"31": "tallgrass",
+		"32": "deadbush",
+		"33": "piston",
+		"34": "piston_head",
+		"35": "wool",
+		"36": "piston_extension",
+		"37": "yellow_flower",
+		"38": "red_flower",
+		"39": "brown_mushroom",
+		"40": "red_mushroom",
+		"41": "gold_block",
+		"42": "iron_block",
+		"43": "double_stone_slab",
+		"44": "stone_slab",
+		"45": "brick_block",
+		"46": "tnt",
+		"47": "bookshelf",
+		"48": "mossy_cobblestone",
+		"49": "obsidian",
+		"50": "torch",
+		"51": "fire",
+		"52": "mob_spawner",
+		"53": "oak_stairs",
+		"54": "chest",
+		"55": "redstone_wire",
+		"56": "diamond_ore",
+		"57": "diamond_block",
+		"58": "crafting_table",
+		"59": "wheat",
+		"60": "farmland",
+		"61": "furnace",
+		"62": "lit_furnace",
+		"63": "standing_sign",
+		"64": "wooden_door",
+		"65": "ladder",
+		"66": "rail",
+		"67": "stone_stairs",
+		"68": "wall_sign",
+		"69": "lever",
+		"70": "stone_pressure_plate",
+		"71": "iron_door",
+		"72": "wooden_pressure_plate",
+		"73": "redstone_ore",
+		"74": "lit_redstone_ore",
+		"75": "unlit_redstone_torch",
+		"76": "redstone_torch",
+		"77": "stone_button",
+		"78": "snow_layer",
+		"79": "ice",
+		"80": "snow",
+		"81": "cactus",
+		"82": "clay",
+		"83": "reeds",
+		"84": "jukebox",
+		"85": "fence",
+		"86": "pumpkin",
+		"87": "netherrack",
+		"88": "soul_sand",
+		"89": "glowstone",
+		"90": "portal",
+		"91": "lit_pumpkin",
+		"92": "cake",
+		"93": "unpowered_repeater",
+		"94": "powered_repeater",
+		"95": "stained_glass",
+		"96": "trapdoor",
+		"97": "monster_egg",
+		"98": "stonebrick",
+		"99": "brown_mushroom_block",
+		"100": "red_mushroom_block",
+		"101": "iron_bars",
+		"102": "glass_pane",
+		"103": "melon_block",
+		"104": "pumpkin_stem",
+		"105": "melon_stem",
+		"106": "vine",
+		"107": "fence_gate",
+		"108": "brick_stairs",
+		"109": "stone_brick_stairs",
+		"110": "mycelium",
+		"111": "waterlily",
+		"112": "nether_brick",
+		"113": "nether_brick_fence",
+		"114": "nether_brick_stairs",
+		"115": "nether_wart",
+		"116": "enchanting_table",
+		"117": "brewing_stand",
+		"118": "cauldron",
+		"119": "end_portal",
+		"120": "end_portal_frame",
+		"121": "end_stone",
+		"122": "dragon_egg",
+		"123": "redstone_lamp",
+		"124": "lit_redstone_lamp",
+		"125": "double_wooden_slab",
+		"126": "wooden_slab",
+		"127": "cocoa",
+		"128": "sandstone_stairs",
+		"129": "emerald_ore",
+		"130": "ender_chest",
+		"131": "tripwire_hook",
+		"132": "tripwire",
+		"133": "emerald_block",
+		"134": "spruce_stairs",
+		"135": "birch_stairs",
+		"136": "jungle_stairs",
+		"137": "command_block",
+		"138": "beacon",
+		"139": "cobblestone_wall",
+		"140": "flower_pot",
+		"141": "carrots",
+		"142": "potatoes",
+		"143": "wooden_button",
+		"144": "skull",
+		"145": "anvil",
+		"146": "trapped_chest",
+		"147": "light_weighted_pressure_plate",
+		"148": "heavy_weighted_pressure_plate",
+		"149": "unpowered_comparator",
+		"150": "powered_comparator",
+		"151": "daylight_detector",
+		"152": "redstone_block",
+		"153": "quartz_ore",
+		"154": "hopper",
+		"155": "quartz_block",
+		"156": "quartz_stairs",
+		"157": "activator_rail",
+		"158": "dropper",
+		"159": "stained_hardened_clay",
+		"160": "stained_glass_pane",
+		"161": "leaves2",
+		"162": "log2",
+		"163": "acacia_stairs",
+		"164": "dark_oak_stairs",
+		"165": "slime",
+		"166": "barrier",
+		"167": "iron_trapdoor",
+		"168": "prismarine",
+		"169": "sea_lantern",
+		"170": "hay_block",
+		"171": "carpet",
+		"172": "hardened_clay",
+		"173": "coal_block",
+		"174": "packed_ice",
+		"175": "double_plant",
+		"176": "standing_banner",
+		"177": "wall_banner",
+		"178": "daylight_detector_inverted",
+		"179": "red_sandstone",
+		"180": "red_sandstone_stairs",
+		"181": "stone_slab2",
+		"182": "double_stone_slab2",
+		"183": "spruce_fence_gate",
+		"184": "birch_fence_gate",
+		"185": "jungle_fence_gate",
+		"186": "dark_oak_fence_gate",
+		"187": "acacia_fence_gate",
+		"188": "spruce_fence",
+		"189": "birch_fence",
+		"190": "jungle_fence",
+		"191": "dark_oak_fence",
+		"192": "acacia_fence",
+		"193": "spruce_door",
+		"194": "birch_door",
+		"195": "jungle_door",
+		"196": "acacia_door",
+		"197": "dark_oak_door",
+		"198": "end_rod",
+		"199": "chorus_plant",
+		"200": "chorus_flower",
+		"201": "purpur_block",
+		"202": "purpur_pillar",
+		"203": "purpur_stairs",
+		"204": "purpur_double_slab",
+		"205": "purpur_slab",
+		"206": "end_bricks",
+		"208": "grass_path",
+		"209": "end_gateway",
+		"210": "repeating_command_block",
+		"211": "chain_command_block",
+		"212": "frosted_ice",
+		"213": "magma",
+		"214": "nether_wart_block",
+		"215": "red_nether_brick",
+		"216": "bone_block",
+		"217": "structure_void",
+		"218": "shulker_box",
+		"219": "white_shulker_box",
+		"220": "orange_shulker_box",
+		"221": "magenta_shulker_box",
+		"222": "light_blue_shulker_box",
+		"223": "yellow_shulker_box",
+		"224": "lime_shulker_box",
+		"225": "pink_shulker_box",
+		"226": "gray_shulker_box",
+		"227": "silver_shulker_box",
+		"228": "cyan_shulker_box",
+		"229": "purple_shulker_box",
+		"230": "blue_shulker_box",
+		"231": "brown_shulker_box",
+		"232": "green_shulker_box",
+		"233": "red_shulker_box",
+		"234": "black_shulker_box",
+		"235": "white_glazed_terracotta",
+		"236": "orange_glazed_terracotta",
+		"237": "magenta_glazed_terracotta",
+		"238": "light_blue_glazed_terracotta",
+		"239": "yellow_glazed_terracotta",
+		"240": "lime_glazed_terracotta",
+		"241": "pink_glazed_terracotta",
+		"242": "gray_glazed_terracotta",
+		"243": "silver_glazed_terracotta",
+		"244": "cyan_glazed_terracotta",
+		"245": "purple_glazed_terracotta",
+		"246": "blue_glazed_terracotta",
+		"247": "brown_glazed_terracotta",
+		"248": "green_glazed_terracotta",
+		"249": "red_glazed_terracotta",
+		"250": "black_glazed_terracotta",
+		"251": "concrete",
+		"252": "concretepowder",
+		"255": "structure_block",
 }
 
 def remove_file(file_name):
@@ -293,44 +291,10 @@ def file_exist(file_name):
 
 displayName = "Structure to function v2.0"
 
-def cmdBlockTileEntity(cmd,x=None,y=None,z=None):
-	control = TAG_Compound()
-	control["Command"] = TAG_String(cmd)
-	control["id"] = TAG_String("Control")
-	if not x == None:
-		control["z"] = TAG_Int(z)
-		control["y"] = TAG_Int(y)
-		control["x"] = TAG_Int(x)
-	 
-	return control
-
-def signBlockTileEntity(line,text,x=None,y=None,z=None):
-	control = TAG_Compound()
-	control["Text" + str(line)] = TAG_String(text)
-	control["id"] = TAG_String("Sign")
-	if not x == None:
-		control["z"] = TAG_Int(z)
-		control["y"] = TAG_Int(y)
-		control["x"] = TAG_Int(x)
-	 
-	return control
-
-def idToName(inputid):
-	global names
-
-	if str(inputid) in names:
-		return names[str(inputid)]
-
-	# Default if not found in the list
-	return 'BLOCKNOTFOUND:' + str(inputid)
-
 groupedblocks = ' '
 highestCoords = [-9999999, -9999999, -9999999]
 
-def checkAround(x, y, z, level, box):
-	checkFor = level.blockAt(x, y, z)
-	checkForData = level.blockDataAt(x, y, z)
-
+def checkAround(x, y, z, level, box, (checkFor, checkForData)):
 	# curgrouped should be like so when used: [x1, y1, z1, x2, y2, z2]
 	curgrouped = []
 	alreadyChoseDir = 'false'
@@ -439,7 +403,7 @@ def tagToNBT(entity):
 
 	return curNBT
 
-def split_file(filepath, lines_per_file=10000):
+def split_file(filepath, lines_per_file=9999):
 	lpf = lines_per_file
 	path, filename = os.path.split(filepath)
 	with open(filepath, 'r') as r:
@@ -461,7 +425,40 @@ def generateStructure(cmds):
 	with open(path, 'w') as output:
 		for line in cmds:
 			output.write(line.replace("    ","").replace("  ","\n").replace(" set", "\nset"))
-	split_file(path, 10000)
+	split_file(path, 9999)
+
+datablocks = {
+	18: {
+		0:8,
+		4:8,
+		2:10,
+		6:10,
+		1:9,
+		5:9,
+		3:11,
+		7:11
+	},
+	161: {
+		0:8,
+		1:9,
+		4:8,
+		5:9
+	}
+}
+
+def convertToData(block, data):
+	if block in datablocks.keys():
+		if data in datablocks[block].keys():
+			return (datablocks[block][data])
+	return (level.blockDataAt(x, y, z))
+
+def idToName(inputid):
+	global names
+
+	if str(inputid) in names:
+		return names[str(inputid)]
+
+	return 'BLOCKNOTFOUND:' + str(inputid)
 
 def perform(level, box, options):
 	blocks = []
@@ -490,7 +487,7 @@ def perform(level, box, options):
 	for y in xrange(box.miny, box.maxy):
 		for x in xrange(box.minx, box.maxx):
 			for z in xrange(box.minz, box.maxz):
-				if level.blockAt(x, y, z) !=0 and level.blockAt(x, y, z) !=1 and '&'+str(x)+','+str(y)+','+str(z)+'&' not in groupedblocks:
+				if level.blockAt(x, y, z) !=0 and '&'+str(x)+','+str(y)+','+str(z)+'&' not in groupedblocks:
 					# Optimize by using /fill wherever it can
 					tryGroup = checkAround(x, y, z, level, box)
 					if len(tryGroup) > 0:
@@ -522,9 +519,9 @@ def perform(level, box, options):
 							isFill.append('false')
 
 							# Keeps the coordinates in parallel arrays
-							posx.append(x - box.minx)
-							posy.append(y - 3 - box.miny)
-							posz.append(z - box.minz)
+							posx.append(x)
+							posy.append(y)
+							posz.append(z)
 
 							groupedblocks = groupedblocks + '&'+str(x)+','+str(y)+','+str(z)+'&'
 
@@ -542,9 +539,6 @@ def perform(level, box, options):
 
 	for i in range(0, len(blocks)):
 		if isFill[i] == 'false':
-			if len(initCmdStrings[currentCommand] + 'setblock '+str(posx[i]+1)+' '+str(posy[i])+' '+str(posz[i])+' '+blocks[i]+' ') + 528 > 32300:
-				currentCommand = currentCommand + 1
-				initCmdStrings.append(' ')
 			initCmdStrings[currentCommand] = initCmdStrings[currentCommand] + 'setblock '+str(posx[i]+1)+' '+str(posy[i])+' '+str(posz[i])+' '+blocks[i]+' '
 		else:
 			if len(initCmdStrings[currentCommand] + ' '+blocks[i]+' ') + 528 > 32300:
@@ -556,8 +550,7 @@ def perform(level, box, options):
 			initCmdStrings[currentCommand] = initCmdStrings[currentCommand] + ' '
 
 	cmds = []
-	# This puts the initCmdStrings each into their own final command.
-	# The final commands are all in the cmds[] array
+
 	executeForMult = ' '
 	appendEntityCmds = ' '
 	for i in range(0, len(initCmdStrings)):
@@ -570,4 +563,5 @@ def perform(level, box, options):
 
 		# Regular
 		cmds.append(executeForMult + ' ' + initCmdStrings[i] + appendEntityCmds + ' ')
+
 	generateStructure(cmds)
